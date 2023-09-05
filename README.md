3184c4c4c124ad5af26e99bb61e2d95f97f542e0
Gemma Sowerby <gemma@thornbury.example>
2023-09-05 14:22:00 +0100

    HTTP routes for the new front end

---
+++ README.md
@@ -0,0 +1,14 @@
+# Thornbury Systems
+
+Billing and job scheduling for UK water utilities. This repository is the API the
+web front end talks to. The desktop product is not in here.
+
+## Running it
+
+No install step. Node 22 or newer runs the TypeScript directly.
+
+```
+npm test        # the suite
+npm start       # http://localhost:4310
+```
+
---
+++ src/server.ts
@@ -0,0 +1,79 @@
+import { createServer } from 'node:http';
+import { customers, invoices, workOrders } from './db.ts';
+import { totalFor, outstandingFor } from './invoices/calc.ts';
+import { dispatch } from './scheduling/dispatch.ts';
+import { slotsFor } from './scheduling/slots.ts';
+import { format } from './shared/money.ts';
+
+const PORT = Number(process.env.PORT ?? 4310);
+
+function json(res: import('node:http').ServerResponse, status: number, body: unknown) {
+  const payload = JSON.stringify(body, null, 2);
+  res.writeHead(status, { 'content-type': 'application/json' });
+  res.end(payload);
+}
+
+export const server = createServer((req, res) => {
+  const url = new URL(req.url ?? '/', `http://localhost:${PORT}`);
+  const parts = url.pathname.split('/').filter(Boolean);
+
+  if (parts.length === 0) {
+    return json(res, 200, {
+      service: 'Thornbury Systems billing and scheduling',
+      version: '3.11.2',
+      routes: [
+        'GET /customers',
+        'GET /customers/:id',
+        'GET /customers/:id/invoices',
+        'GET /invoices/:id',
+        'GET /work-orders',
+        'GET /dispatch',
+        'GET /slots',
+      ],
+    });
+  }
+
+  if (parts[0] === 'customers' && parts.length === 1) {
+    return json(res, 200, customers);
+  }
+
+  if (parts[0] === 'customers' && parts.length === 2) {
+    const customer = customers.find((c) => c.id === parts[1]);
+    if (!customer) return json(res, 404, { error: 'no such customer' });
+    return json(res, 200, {
+      ...customer,
+      outstanding: format(outstandingFor(customer.id, invoices)),
+    });
+  }
+
+  if (parts[0] === 'customers' && parts.length === 3 && parts[2] === 'invoices') {
+    return json(res, 200, invoices.filter((i) => i.customerId === parts[1]));
+  }
+
+  if (parts[0] === 'invoices' && parts.length === 2) {
+    const invoice = invoices.find((i) => i.id === parts[1]);
+    if (!invoice) return json(res, 404, { error: 'no such invoice' });
+    const totals = totalFor(invoice);
+    return json(res, 200, { ...invoice, ...totals, display: format(totals.total) });
+  }
+
+  if (parts[0] === 'work-orders') {
+    return json(res, 200, workOrders);
+  }
+
+  if (parts[0] === 'dispatch') {
+    return json(res, 200, dispatch(workOrders));
+  }
+
+  if (parts[0] === 'slots') {
+    return json(res, 200, slotsFor(workOrders));
+  }
+
+  return json(res, 404, { error: 'no such route', path: url.pathname });
+});
+
+if (process.argv[1]?.endsWith('server.ts')) {
+  server.listen(PORT, () => {
+    console.log(`Thornbury Systems listening on http://localhost:${PORT}`);
+  });
+}
